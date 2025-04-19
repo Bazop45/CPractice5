827b05d843b75ae784dc391f8fee3ec6e95b7903
Bazop45 <80447574+Bazop45@users.noreply.github.com>
2025-04-19 08:49:14 +0300

    1e.c

--- 1e.c
+++ 1e.c
@@ -3,8 +3,7 @@
 
 int main() {
     char alphabet[] = "abcdefghijklmnopqrstuvwxyz"; // Alphabet array
-    char letter = 'l'; // Letter, that's need to find
-    
+    char letter = 's'; // Letter, that's need to find
     int index = -1;
 
     for (int i = 0; i < strlen(alphabet); i++) {
@@ -15,9 +14,9 @@ int main() {
     }
 
     if (index != -1) {
-        printf("Буква '%c' находится на позиции %d в алфавите\n", letter, index); // Print letter and letter's index
+        printf("Letter '%c' found on %d pos of alphabet\n", letter, index); // Print letter and letter's index
     } else {
-        printf("Буква '%c' не найдена в алфавите\n", letter); // Print failure and missed letter
+        printf("Letter '%c' not found in alphabet\n", letter); // Print failure and missed letter
     }
 
     return 0;
